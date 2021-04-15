b017290706e6a78feb1d18098f872434b0107507
Shomee1015 <mrimmoaee@gmail.com>
2021-04-15 09:33:56 +0600

    work experience and animated text design done

---
+++ js/app.js
@@ -0,0 +1,28 @@
+const texts = [
+  "Frontend Software Engineer",
+  "Web Designer",
+  "React Developer",
+  "Javascript Developer",
+  "Programming Blogger",
+];
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type() {
+  if (count === texts.length) {
+    count = 0;
+  }
+
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+
+  document.querySelector(".typing").textContent = letter;
+  if (letter.length === currentText.length) {
+    count++;
+    index = 0;
+  }
+  setTimeout(type, 400);
+})();
